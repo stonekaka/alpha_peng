6059ea23a1adc35c25d7888c14b2e7f7bec8b971
stonekaka <stoneforfun@gmail.com>
2015-11-17 18:59:22 +0800

    add dm init script

--- ap/kernel/install_dm202.sh
+++ ap/kernel/install_dm202.sh
@@ -3,6 +3,7 @@
 DIR=/home/op/gitlab/twsz-openwrt-2.6.36/package/ezlink/ezwrt_api/files/
 
 /bin/cp pengwifi.ko $DIR/lib/modules/
+/bin/cp pengwifi $DIR/etc/init.d/
 /bin/cp ../user/pwf $DIR/usr/sbin/
 /bin/cp ../../libwebsockets/libwebsockets.so $DIR/usr/lib/
 rm -rf /home/op/gitlab/twsz-openwrt-2.6.36/build_dir/target-mipsel_24kec+dsp_uClibc-0.9.33.2/ezwrt-api
